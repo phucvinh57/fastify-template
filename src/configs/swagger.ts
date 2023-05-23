f5804d056272427f21cccf16dd46636d01efe13a
phucvinh57 <npvinh0507>
2023-05-23 18:54:00 +0700

    docs(swaggers): modify swagger configs

--- src/configs/swagger.ts
+++ src/configs/swagger.ts
@@ -1,13 +1,14 @@
 import { FastifyDynamicSwaggerOptions } from '@fastify/swagger';
 import { FastifySwaggerUiOptions } from '@fastify/swagger-ui';
+import pkg from '../../package.json';
 
 export const swaggerConfig: FastifyDynamicSwaggerOptions = {
     openapi: {
         info: {
-            title: 'API docs',
-            summary: 'Swagger documentation',
-            version: '0.1.0',
-            license: { name: 'ISC' }
+            title: pkg.name,
+            version: pkg.version,
+            license: { name: 'ISC' },
+            summary: pkg.description
         },
         externalDocs: {
             url: 'https://swagger.io',
@@ -21,11 +22,5 @@ export const swaggerUIConfig: FastifySwaggerUiOptions = {
     uiConfig: {
         deepLinking: false
     },
-    staticCSP: true,
-    transformStaticCSP: (header) => header,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformSpecification: (swaggerObject, _request, _reply) => {
-        return swaggerObject;
-    },
-    transformSpecificationClone: true
+    staticCSP: false
 };
