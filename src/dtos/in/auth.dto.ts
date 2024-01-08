e34ad4649556b52f76bfad1b399b4558258d6fa0
phucvinh57 <npvinh0507>
2024-01-08 13:18:29 +0700

    chore: change dto folder structure

--- src/dtos/in/auth.dto.ts
+++ src/dtos/in/auth.dto.ts
@@ -1,8 +1,6 @@
 import { MIN_EMAIL_LENGTH, MIN_PASSWORD_LENGTH } from '@constants';
 import { Static, Type } from '@sinclair/typebox';
 
-// See https://github.com/sinclairzx81/typebox
-
 export const AuthInputDto = Type.Object({
     email: Type.String({ minLength: MIN_EMAIL_LENGTH }),
     password: Type.String({ minLength: MIN_PASSWORD_LENGTH })
